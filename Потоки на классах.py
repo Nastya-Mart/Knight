d54b663dfc1960d8f9abbeeaf4cbed0879dc607e
Nastya-Mart <nastya.mart749@gmail.com>
2024-12-16 18:21:38 +0300

    Add files via upload

---
+++ Потоки на классах.py
@@ -0,0 +1,37 @@
+import threading
+import time
+
+class Knight(threading.Thread):
+
+    def __init__ (self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.enemies = 100
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        days = 0
+
+        while self.enemies > 0:
+            time.sleep(1)
+            days += 1
+            self.enemies -= self.power
+
+            if self.enemies < 0:
+                self.enemies = 0
+
+            print(f'{self.name} сражается {days} день..., осталось {self.enemies} воинов.')
+        print(f'{self.name} одержал победу спустя {days} дней(дня)!')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
+
+
